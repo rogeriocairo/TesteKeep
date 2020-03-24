ac5c8e547ef7305ebbbec573e8b9479352b546ee
Rogério Cairo do Carmo <carmoaguiar@hotmail.com>
2020-03-24 14:00:24 -0300

    Adicionando o projeto par impar

---
+++ ParImpar/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ParImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Entre com um numero inteiro: ");
+            var numero = int.Parse( Console.ReadLine());
+            Console.WriteLine($"Invertido: {ParImpar(numero)}");
+            Console.ReadLine();
+        }
+
+        private static string ParImpar(int numero)
+        {
+            return numero % 2 == 0 ? "Par" : "Impar";
+        }        
+    }
+}
